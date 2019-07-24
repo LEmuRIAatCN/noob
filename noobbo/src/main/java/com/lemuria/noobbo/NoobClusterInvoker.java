36b618d4e252fa6266d5d6311bb7633c214fe034
LEmuRIA <yylryyzx@gmail.com>
2019-07-24 23:38:08 +0800

    12

---
+++ noobbo/src/main/java/com/lemuria/noobbo/NoobClusterInvoker.java
@@ -0,0 +1,75 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.lemuria.noobbo;
+
+import org.apache.dubbo.common.URL;
+import org.apache.dubbo.common.logger.Logger;
+import org.apache.dubbo.common.logger.LoggerFactory;
+import org.apache.dubbo.rpc.Invocation;
+import org.apache.dubbo.rpc.Invoker;
+import org.apache.dubbo.rpc.Result;
+import org.apache.dubbo.rpc.RpcException;
+import org.apache.dubbo.rpc.cluster.Cluster;
+import org.apache.dubbo.rpc.cluster.Directory;
+import org.apache.dubbo.rpc.cluster.LoadBalance;
+import org.apache.dubbo.rpc.cluster.support.AbstractClusterInvoker;
+import org.apache.dubbo.rpc.cluster.support.wrapper.MockClusterInvoker;
+
+import java.util.List;
+
+/**
+ *
+ *
+ */
+public class NoobClusterInvoker<T> implements Invoker<T> {
+
+    private static final Logger logger = LoggerFactory.getLogger(NoobClusterInvoker.class);
+
+    private final Directory<T> directory;
+
+    private final Invoker<T> invoker;
+
+    public NoobClusterInvoker(Directory<T> directory, Invoker<T> invoker) {
+        this.directory = directory;
+        this.invoker = invoker;
+    }
+
+    @Override
+    public Class<T> getInterface() {
+        return null;
+    }
+
+    @Override
+    public Result invoke(Invocation invocation) throws RpcException {
+        return directory.list(invocation).get(0).invoke(invocation);
+    }
+
+    @Override
+    public URL getUrl() {
+        return null;
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return false;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
